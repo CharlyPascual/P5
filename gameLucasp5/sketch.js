3d726f38e7d956bc3e008149f8d77c005d248c93
charly <charlyp@scual.es>
2014-12-07 03:15:34 +0100

    Activar botón de sonido , no funciona el score

--- gameLucasp5/sketch.js
+++ gameLucasp5/sketch.js
@@ -5,6 +5,9 @@
 
 /////////////////////////////////////////////////
 var score = 0;
+
+//load buttos
+var input, b, soundplay;
 ////////////////////////
 var numberOfAnimals = 9; // number of photos to show
 var numberIndice = 8; // 0,1,2....
@@ -12,7 +15,7 @@ var numberLoadAnimals = 9; // number of photos to choose
 
 //load index
 var numbers = [];
-
+h = 0; //indexador a 0
 //loading photos
 var animals = [];
 var path = "animals/animal";
@@ -22,7 +25,6 @@ var extension = ".jpg";
 sample = [];
 var trackNameList = ["0lion.wav", "1elephant.wav", "2tiger.wav", "3gorila.wav", 
   "4wolf.wav", "5bear.wav", "6buffalo.wav", "7horse.wav", "8lince.wav"]
-h = 0;
 /////////////////////////////////////////////////
 //Positions photos
 var margin = (1024 / 100) * 2;
@@ -36,23 +38,23 @@ var marginY = 100;
 /////////////////////////////////////////////////
 function preload() {
   //load images
-  for (var i = 0; i != numberOfAnimals; print("IdxP: #" + i)
+  for (var i = 0; i != numberOfAnimals; print(path + i)
     , animals[i] = loadImage(path + i++ + extension));
   //load sounds  
-  for (var h = 0; h != numberOfAnimals; print("IdxS: #" + h)  
+  for (var h = 0; h != numberOfAnimals; print(trackNameList[h])  
     ,sample[h] = loadSound("sounds/" + trackNameList[h++]));
 }
 
 
 function setup() {
   createCanvas(1024, 680);
-  fill(0);
-  textSize(28);
+  //fill(0);
+  //textSize(28);
   background(255);
   //sample[int(random(5))].loop();
   
   generate(9);
-  
+  sonar()
 }
 //http://stackoverflow.com/questions/15584716/unique-random-number-generator-javascript
 //the while loop is a do/while. Which means it will run the inline 
@@ -61,6 +63,7 @@ function setup() {
 //in the array. indexOf(n) returns the integer key for the value of 
 //n if it exists in the array, otherwise it returns -1. Thus, running 
 //the loop again if indexOf does not return -1.
+
 function generate(length)
 {
     
@@ -68,7 +71,7 @@ function generate(length)
     for(var i=0; i<length; i++)
     {
         do
-            n = Math.floor(Math.random()*9+1);
+            n = Math.floor(Math.random()*9);
         while(numbers.indexOf(n) !== -1)
             print(n);
        numbers[i] = n;
@@ -76,17 +79,29 @@ function generate(length)
     return numbers;
 }
 
+function sonar(){
+    fill(204);
+  noStroke();
+  rect(0, 0, 182, 50); // fondo del marcador
+ b = createButton('SOUNDS');
+ b.position(int(widthX+margin*2),int(margin));
+ b.mousePressed(animalSound);
+ 
+}
+
 function draw() {
   //background(255);
   //display photo
-  displayPhoto();
+ displayPhoto();
   //display score
-  text("SCORE: " + score, margin, margin * 2);
+   fill(0);
+  textSize(28);
+  text(score, margin, margin * 2);
 }
 
 function displayPhoto() {
-    image(animals[currentPhoto++], onSeeX, marginY, widthX, heightY);
-
+    image(animals[numbers[h++]], onSeeX, marginY, widthX, heightY);
+    currentPhoto++;
     onSeeX += (widthX + margin);
     if ((onSeeX + widthX) >= (width - margin)) {
       onSeeX = margin;
@@ -98,6 +113,111 @@ function displayPhoto() {
     }
 }
 
+function animalSound(){
+  selectionA = int(random(0, numberOfAnimals)); //seleciono un sonido de animal
+    print("sonido selectionA: [" + trackNameList[selectionA] + "]");
+    switch(selectionA)
+    {
+    case 0:
+      //sample[selectionA].cue(0);
+      sample[selectionA].play();
+      break;
+    case 1:
+      //sample[selectionA].cue(0);
+      sample[selectionA].play();
+      break;
+    case 2:
+      //sample[selectionA].cue(0);
+      sample[selectionA].play();
+      break;
+    case 3:
+      //sample[selectionA].cue(0);
+      sample[selectionA].play();
+      break;
+    case 4:
+     // sample[selectionA].cue(0);
+      sample[selectionA].play();
+      break;
+    case 5:
+      //sample[selectionA].cue(0);
+      sample[selectionA].play();
+      break;
+    case 6:
+      //sample[selectionA].cue(0);
+      sample[selectionA].play();
+      break;
+    case 7:
+      //sample[selectionA].cue(0);
+      sample[selectionA].play();
+      break;
+    case 8:
+      //sample[selectionA].cue(0);
+      sample[selectionA].play();
+      break;
+    case 9:
+      //sample[selectionA].cue(0);
+      sample[selectionA].play();
+      break;
+    }
+}
+function mousePressed() {
+  //float d = dist(margin+widthX/2, 100+heightY/2, mouseX, mouseY);
+  //onSeeX += widthX + margin;
+  println("x: " + mouseX + " y: " + mouseY);
+ 
+  print(score);
+  if ((mouseX >= margin && mouseX <= widthX + margin)
+    &&(mouseY >= marginY && mouseY <= marginY + heightY)
+    && (selectionA === 0)) {
+    score += 1;
+  
+  }
+  if ((mouseX >= 2*margin + widthX)
+    && (mouseX <= 2 * (widthX + margin))
+    && (mouseY >= marginY && mouseY <= marginY + heightY)
+    && (selectionA == 1)) {
+    score += 1;
+    
+  }
+  if ((mouseX >= 2*margin + 2*widthX + margin)
+    && (mouseX <= 3 * (widthX + margin))
+    && (mouseY >= marginY && mouseY <= marginY + heightY)
+    && (selectionA == 2)) {
+    score += 1;
+  }
+
+  if ((mouseX >= margin && mouseX <= widthX + margin) 
+    &&(mouseY >= marginY + heightY + margin && mouseY <= marginY + 2*heightY + margin)
+    && (selectionA == 3)) {
+    score += 1;
+  
+  }
+  if ((mouseX >= 2*margin + widthX) && (mouseX <= 2 * (widthX + margin))
+    &&(mouseY >= marginY + heightY + margin && mouseY <= marginY + 2*heightY + margin)
+    && (selectionA == 4)) {
+    score += 1;
+  }
+  if ((mouseX >= 3*margin + 2*widthX)  && (mouseX <= 3 * (widthX + margin))
+    &&(mouseY >= marginY + heightY + margin && mouseY <= marginY + 2*heightY + margin)
+    && (selectionA == 5)) {
+    score += 1;
+  }/////
+  if ((mouseX >= margin && mouseX <= widthX + margin) 
+    &&(mouseY >= marginY + 2 * (heightY + margin) && mouseY <= marginY + 3*heightY + 2*margin)
+    && (selectionA == 6)) {
+    score += 1;
+  }
+  if ((mouseX >= 2*margin + widthX) && (mouseX <= 2 * (widthX + margin))
+    &&(mouseY >= marginY + 2 * (heightY + margin) && mouseY <= marginY + 3*heightY + 2*margin)
+    && (selectionA == 7)) {
+    score += 1;
+  }
+  if ((mouseX >= 3*margin + 2*widthX) && (mouseX <= 3 * (widthX + margin))
+    &&(mouseY >= marginY + 2 * (heightY + margin) && mouseY <= marginY + 3*heightY + 2*margin)
+    && (selectionA == 8)) {
+    score += 1;
+  }
+}//generarlo para cada foto 9 en total
 
 
 
